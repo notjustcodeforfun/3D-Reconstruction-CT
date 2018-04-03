6f1c5ce0b90e47b19c5d66ef8e3986762b1d5b58
notjustcodeforfun <37809065+notjustcodeforfun@users.noreply.github.com>
2018-04-03 15:46:57 +0200

    preprocressing apdated

--- src/param.m
+++ src/param.m
@@ -5,23 +5,27 @@ function output = param
 
 
 % ------------------------ Parameter von Umgebung
-output.dataTyp         = 1;         % 1 :CLSM  0:CT    
-output.Zyklen          = 10;        % Anzahl von Wiederholung der for-Schleife
+output.dataTyp         = 1;        % 1 :CLSM  0:CT    
+output.Zyklen          = 10;       % Anzahl von Wiederholung der for-Schleife
 output.SwitchVolume    = true;     % Volumen Filter
 output.SwitchPorenV    = false;    % Porenverteilung
 output.ShowDetails     = true;     % Anzeigen des aktuellen Zustand 
-output.SwitchVTK       = false;
-output.Elementsize     = 3;        % Gr??e der Strukturelement, Einheit [voxel]
+output.Elementsize     = 3;        % Groe?e der Strukturelement, Einheit [voxel]
 output.scaling         = 1;        % Massstab x,y
 output.spacing         = 1;        % Massstab z  
 output.MinVolume       = 10;       % Min. Volumen von Volumenfilter
-output.por             = 1;        % Anteil des zu verwendenen Pixel
 output.skip            = 1;        % Bild neu aufbauen
 output.skip_thresh     = 10;
-output.Kth             = 1.0;        % multi. coeff. to threshhold adjustment
+output.Kth             = 1.0;      % multi. coeff. to threshhold adjustment
 output.Kp              = 1.2;      % Propotionscoeffizient der Regelung
 output.Ki              = 0.0;      % I Glied der Regler
 output.ab              = 0.005;    % Abbruch Schwellwert
+% ------------------------ Bilddrehung und Bildbeschneidung
+output.rot             = 0;        % Rotation
+output.x1              = 0;
+output.x2              = 1024;     % default: end
+output.y1              = 0;
+output.y2              = 1024;     % default: end
 % ------------------------ Sollwert
 output.porositaet_soll = 0.8;
 % ------------------------ Initialization
