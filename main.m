27d0573d08c0e957a7c51dc7b5b7da6256196385
notjustcodeforfun <37809065+notjustcodeforfun@users.noreply.github.com>
2018-04-09 12:07:28 +0200

    added pool calculation

--- main.m
+++ main.m
@@ -1,14 +1,59 @@
 close all;clear
-addpath('C:\Users\fuxia\Documents\Thesis\code\prototyp\Data','BaSiC-master','BaSiC-master\dcttool','skel2graph3d','skeleton3d','src')
+addpath('D:\Projekte\Poroese_Materialien\Matlab\Xiaoyu\3D-Reconstruction-CT-master','BaSiC-master','BaSiC-master\dcttool','skel2graph3d','skeleton3d','src')
+formatOut = 'yymmddhhMM';
+startzeit = datestr(datetime, formatOut);
 para = param;
+
+            
 load(para.datapath);
+
 img_stack_after = bildvorverarbeitung(img_stack, para);
-for times = 1:para.Zyklen
-    img_bin = bildverarbeitung(img_stack_after, para);
-    merkmal = merkmalExtraktion(img_bin, para);
-    if abs(merkmal.porositaet-para.porositaet_soll)<para.ab
-        break
+
+if para.switchParampool == 0
+    for times = 1:para.Zyklen
+        img_bin = bildverarbeitung(img_stack_after, para);
+        merkmal = merkmalExtraktion(img_bin, para);
+        if abs(merkmal.porositaet-para.porositaet_soll)<para.ab
+            break
+        end
+        para = vergleichSWG(merkmal, para);
     end
-    para = vergleichSWG(merkmal, para);
+    showErgebnis(img_bin,para,merkmal)
+elseif para.switchParampool == 1 % Parameterpool 
+    % Erste Zeile Auswertungstabelle generieren: 
+    evaluation = {'Probe:'};
+    parameterNames = fieldnames(para.pool); %Namen der Parameter im Modellpool
+    filename = split(para.datapath, '.'); %Filename f .xlsx erzeugen.
+    for i = 1:numel(parameterNames)
+        evaluation{end+1} = parameterNames{i};
+    end
+    evaluation = [evaluation, 'Porositaet:', 'Anzahl Objekte:', 'Knotenanzahl:', 'Endpunkte %:', '3 Stege %:', '4 Stege %:', '5 Stege %:', 'Durchschnittliche Steglänge:'];
+    
+    %% Berechnen aller Kombinationen
+    for i = 1:length(para.combinations)
+        % Neue Parameter der Berechnung zuweisen und in Auswertungstabelle
+        % speichern
+        evaluationDatarow = {para.datapath};
+        for j = 1:numel(parameterNames)
+            para.(parameterNames{j}) = para.combinations(j,i); 
+            evaluationDatarow = [evaluationDatarow, para.combinations(j,i)];
+        end
+        img_bin = bildverarbeitung(img_stack_after, para);
+        merkmal = merkmalExtraktion(img_bin, para);
+        % Ergebnisse hinzufuegen
+        evaluationDatarow = [evaluationDatarow, showErgebnis(img_bin,para,merkmal)];
+        evaluation = [evaluation; evaluationDatarow];
+
+        %% Fortschritt dokumentieren!
+        if mod(i,10) == 0
+            fprintf("%i/%i gerechnet!\n",i,size(para.combinations,2));
+            %Als xls speichern
+            xlswrite(strcat(filename{1}, '_', startzeit,'.xlsx'), evaluation);
+        end
+    end
+    xlswrite(strcat(filename{1}, '_', startzeit,'.xlsx'), evaluation);
+    paraCell = fieldnames(para);
+    paraCell = [paraCell, struct2cell(para)];
+    xlswrite(strcat(filename{1}, '_', startzeit,'.xlsx'), paraCell, 'Parameters');
+    
 end
-showErgebnis(img_bin,para,merkmal)
--- src/param.m
+++ src/param.m
@@ -3,14 +3,22 @@ function output = param
 % ----------------------  Parameter einstellen ------------------------------
 % ***************************************************************************
 
+% ------------------------ Parameterpool
+% Wenn der Parameterpool verwendet wird, werden alle Kombinationen des
+% parameterpools durchgerechnet.
+output.switchParampool    = 0;
+output.pool.Kth        = 0.8:0.1:1.0;
+output.pool.Elementsize= 2:1:3;
+output.pool.MinVolume  = 1:10:20;
 
 % ------------------------ Parameter von Umgebung
-output.dataTyp         = 1;        % 1 :CLSM  0:CT    
+output.dataTyp         = 1;        % 1 :CLSM  0:CT   2:Generierte Struktur manipuliert 
 output.Zyklen          = 1;       % Anzahl von Wiederholung der for-Schleife
-output.SwitchBaSiC     = true;     % BaSic filter (Bildvorverarbeitung)
-output.SwitchVolume    = true;     % Volumen Filter
-output.SwitchPorenV    = false;    % Porenverteilung
-output.ShowDetails     = true;     % Anzeigen des aktuellen Zustand 
+output.SwitchBaSiC     = 1;     % BaSic filter (Bildvorverarbeitung)
+output.SwitchVolume    = 1;     % Volumen Filter
+output.SwitchPorenV    = 0;    % Porenverteilung
+output.switchDOG       = 1; 
+output.ShowDetails     = 1;     % Anzeigen des aktuellen Zustand 
 output.Elementsize     = 3;        % Groe?e der Strukturelement, Einheit [voxel]
 output.scaling         = 1;        % Massstab x,y
 output.spacing         = 1;        % Massstab z  
@@ -22,21 +30,40 @@ output.Kp              = 1.3;      % Propotionscoeffizient der Regelung
 output.Ki              = 0.1 ;       % I Glied der Regler
 output.ab              = 0.005;    % Abbruch Schwellwert
 % ------------------------ Bilddrehung und Bildbeschneidung
+output.switchRotCut    = 1;
 output.rot             = 0;        % Rotation
-output.x1              = 1;
-output.x2              = 1024;     % default: end
-output.y1              = 1;
-output.y2              = 1024;     % default: end
+output.x1              = 501;
+output.x2              = 700;     % default: end
+output.y1              = 501;
+output.y2              = 700;     % default: end
+output.z1              = 11;
+output.z2              = 210;
 % ------------------------ Sollwert
 output.porositaet_soll = 0.8;
 % ------------------------ Initialization
 output.diff_sum        = 0;        % Integral von Porositaetsabweichung   
 
-if output.dataTyp
-    output.datapath = 'img_stack.mat';
+%% Bildstapel laden:
+if output.dataTyp == 1
+    output.datapath = 'C:\Users\fuxia\Documents\Thesis\code\prototyp\Data\img_stack.mat';
     fprintf(['Datetype: ','CLSM\n']);
+elseif output.dataTyp == 2
+    output.datapath = 'D:\Projekte\Poroese_Materialien\Data\Vorverarbeitet\GenerierteStrukturen\GenerierteStrukturen_DOG1.mat';
+    fprintf(['Datetype: ','Generierte Struktur 1 manipuliert\n']);    
 else
     output.datapath = 'img_stack_CT.mat';
     fprintf(['Datetype: ','CT\n']);
 
+end
+
+%% Parameterpool generieren:
+if output.switchParampool == 1
+    poolCell = struct2cell(output.pool);
+    output.combinations = poolCell{1};
+    for i = 1:size(poolCell,1)-1
+        y = repmat(output.combinations',size(poolCell{i+1},2),1)';
+        z = reshape(repmat(poolCell{i+1},size(output.combinations,2),1),1,[]);
+        output.combinations = [y; z];
+    end
+    clear i y z poolCell;
 end
